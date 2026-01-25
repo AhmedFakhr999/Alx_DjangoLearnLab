1e869cf2f4af60b4a48f604aed46840163e870b1
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2026-01-25 15:13:48 +0200

    adding the return self.name

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,12 +4,18 @@ from django.db import models
 
 class Author(models.Model):
     name= models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
     
     
     
     
 class Book(models.Model):
     title=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
     author=models.ForeignKey(Author, on_delete=models.CASCADE)
     
     
@@ -17,10 +23,16 @@ class Book(models.Model):
 class Library(models.Model):
     
     name= models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
     books=models.ManyToManyField(Book,related_name='library')
     
 class Librarian(models.Model):
     name= models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
     library= models.OneToOneField(Library,on_delete=models.CASCADE)
 
 
