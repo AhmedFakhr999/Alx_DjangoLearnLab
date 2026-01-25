0f7d65491ae3480d4722ffbb56c66068d5b4c5cf
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2026-01-25 18:47:40 +0200

    udpating django models with task 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView  # Add TemplateView here
-from .models import Book, Library, Author
+from .models import Book
+from .models import Library
+from .models import Author
 
 # Function-based view to list all books
 def list_books(request):
