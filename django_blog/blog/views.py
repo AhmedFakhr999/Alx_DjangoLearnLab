1d31193853ccf8c1266a77346d4ba6ad9b303c61
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2026-03-05 22:54:10 +0200

    Adding Task 1 try

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
 def index(request):
     posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blog/index.html', {'posts': posts})
+
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        # Simple profile update logic
+        request.user.email = request.POST.get('email')
+        request.user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
