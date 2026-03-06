baedea674a1ca491c83fafbdb878fc95753ade9c
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2026-03-06 17:10:11 +0200

    Initialize the taks 2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,9 @@ from .models import Post
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 def index(request):
     posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blog/index.html', {'posts': posts})
@@ -29,3 +31,43 @@ def profile(request):
         request.user.save()
         return redirect('profile')
     return render(request, 'blog/profile.html')
+
+
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html' # Default is post_list.html
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
